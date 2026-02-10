ce667803751ec49f3de728ed8b1d27c8a91f7f5b
abelcrvg <anonymopro@gmail.com>
2026-02-09 22:23:19 -0300

    fix rota feed

--- server.py
+++ server.py
@@ -1,9 +1,16 @@
 from flask import Flask, send_file
+import os
 
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return "Servidor online."
+
 @app.route("/feed")
 def feed():
-    return send_file("feed.xml", mimetype="application/rss+xml")
+    caminho = os.path.join(os.path.dirname(__file__), "feed.xml")
+    return send_file(caminho, mimetype="application/rss+xml")
 
-app.run(host="0.0.0.0", port=8000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
